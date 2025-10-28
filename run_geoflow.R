76d991c432ecb4c9b55ccf295975c1b513eb2979
eblondel <emmanuel.blondel1@gmail.com>
2025-10-28 08:39:16 +0100

    update geoflow with few fixes

--- run_geoflow.R
+++ run_geoflow.R
@@ -1,5 +1,6 @@
 # run_geoflow.R
 args <- commandArgs(trailingOnly = TRUE)
 cat("Configuration file:", args[1], "\n")
-result <- geoflow::executeWorkflow(file = args[1], dir = ".")
+library(geoflow)
+result <- executeWorkflow(file = args[1], dir = ".")
 cat(result, "\n")
